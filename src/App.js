11dc8ffbdda42b27b0f173968de6b910acb03d12
Luciana Cândido <lucianacscandido@gmail.com>
2020-12-10 21:56:19 -0300

    contador feito com react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, SetCount] = useState(1)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Counter</h1>
+        <h1>{count}</h1> {/* usando js dentro do jsx (react) tem que ser entre {}*/}
+        <button onClick={() => SetCount(count + 1)}>+</button>
+        <button onClick={() => SetCount (0)}>reset</button>
+        <button onClick={() => SetCount(count > 0 ? count - 1 : 0)}>-</button>
     </div>
   );
 }
